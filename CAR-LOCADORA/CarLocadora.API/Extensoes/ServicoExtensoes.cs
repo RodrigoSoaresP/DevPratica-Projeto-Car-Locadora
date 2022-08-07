d8ded9fccd02ea6061df561c00cc9c025859856c
Rodrigo Soares <rodrigosoares7@gmail.com>
2022-08-06 23:24:06 -0300

    Adicionado o Scoped

--- CAR-LOCADORA/CarLocadora.API/Extensoes/ServicoExtensoes.cs
+++ CAR-LOCADORA/CarLocadora.API/Extensoes/ServicoExtensoes.cs
@@ -1,4 +1,9 @@
 ﻿
+using CarLocadora.Negocio.CategoriaNegocio;
+using CarLocadora.Negocio.ClienteNegocio;
+using CarLocadora.Negocio.FormaPagamentoNegocio;
+using CarLocadora.Negocio.UsuarioNegocio;
+using CarLocadora.Negocio.VeiculoNegocio;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarLocadora.API.Extensoes
@@ -12,7 +17,11 @@ namespace CarLocadora.API.Extensoes
             string connectionString = "Data Source=localhost,1433;User ID=sa;Password=senha@1234;Initial Catalog=DBCarLocadora;";
 
             services.AddDbContext<Infra.Entity.Context>(item => item.UseSqlServer(connectionString));
-
+            services.AddScoped<ICategoriaNegocio, CategoriaNegocio>();
+            services.AddScoped<IClienteNegocio, ClienteNegocio>();
+            services.AddScoped<IFormaPagamentoNegocio, FormaPagamentoNegocio>();
+            services.AddScoped<IUsuarioNegocio, UsuarioNegocio>();
+            services.AddScoped<IVeiculoNegocio, VeiculoNegocio>();
         }
 
 
