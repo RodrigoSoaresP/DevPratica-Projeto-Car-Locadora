3f4946c7e628ff4461e0bd2181e6858c3f830c6d
Rodrigo Soares <rodrigosoares7@gmail.com>
2022-08-24 21:32:37 -0300

    Criação do módulo de manutenção de veiculos

---
+++ CAR-LOCADORA/CarLocadora.Front/Controllers/ManutencaoVeiculoController.cs
@@ -0,0 +1,231 @@
+﻿using CarLocadora.Front.Models;
+using CarLocadora.Front.Servico;
+using CarLocadora.Modelo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+
+namespace CarLocadora.Front.Controllers
+{
+    public class ManutencaoVeiculoController : Controller
+    {
+        private readonly IOptions<DadosBase> _dadosBase;
+        private readonly IApiToken _apiToken;
+        public ManutencaoVeiculoController(IOptions<DadosBase> dadosBase, IApiToken apiToken)
+        {
+            _dadosBase = dadosBase;
+            _apiToken = apiToken;
+
+        }
+        // GET: ManutencaoVeiculoController
+        public async Task<IActionResult> Index(string? mensagem = null, bool sucesso = true)
+        {
+            if (sucesso)
+                TempData["sucesso"] = mensagem;
+            else
+                TempData["erro"] = mensagem;
+
+            HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiToken.Obter());
+
+            HttpResponseMessage response = client.GetAsync($"{_dadosBase.Value.API_URL_BASE}ManutencaoVeiculo").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+                return View(JsonConvert.DeserializeObject<List<ManutencaoVeiculoModel>>(conteudo));
+            }
+            else
+            {
+                throw new Exception("Não foi possível carregar as informações!");
+            }
+        }
+
+        // GET: ManutencaoVeiculoController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ManutencaoVeiculoController/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Veiculos = CarregarVeiculos();
+            return View();
+        }
+
+        // POST: ManutencaoVeiculoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] ManutencaoVeiculoModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    HttpClient client = new();
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiToken.Obter());
+
+                    HttpResponseMessage response = client.PostAsJsonAsync($"{_dadosBase.Value.API_URL_BASE}ManutencaoVeiculo", model).Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction(nameof(Index), new { mensagem = "Registro criado!", sucesso = true });
+                    }
+                    else
+                    {
+                        throw new Exception("Não foi possível carregar as informações!");
+                    }
+                }
+                else
+                {
+                    TempData["erro"] = "Algum campo deve estar faltando o seu preenchimento!";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["erro"] = "Algum erro aconteceu " + ex.Message;
+                return View();
+            }
+        }
+
+        // GET: ManutencaoVeiculoController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiToken.Obter());
+
+            HttpResponseMessage response = client.GetAsync($"{_dadosBase.Value.API_URL_BASE}ManutencaoVeiculo?Id={id}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.Veiculos = CarregarVeiculos();
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+                return View(JsonConvert.DeserializeObject<ManutencaoVeiculoModel>(conteudo));
+            }
+            else
+            {
+                throw new Exception("Não foi possível carregar as informações!");
+
+            }
+        }
+
+        // POST: ManutencaoVeiculoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromForm] ManutencaoVeiculoModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    HttpClient client = new();
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiToken.Obter());
+
+                    HttpResponseMessage response = client.PutAsJsonAsync($"{_dadosBase.Value.API_URL_BASE}ManutencaoVeiculo", model).Result;
+
+                    if (response.IsSuccessStatusCode)
+                        return RedirectToAction(nameof(Index), new { mensagem = "Registro editado!", sucesso = true });
+                    else
+                        throw new Exception("Não foi possível carregar as informações!");
+                }
+                else
+                {
+                    TempData["erro"] = "Algum campo deve estar faltando o seu preenchimento!";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["erro"] = "Algum erro aconteceu " + ex.Message;
+                return View();
+            }
+        }
+
+
+        // GET: ManutencaoVeiculoController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                HttpClient client = new();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiToken.Obter());
+
+                HttpResponseMessage response = client.DeleteAsync($"{_dadosBase.Value.API_URL_BASE}ManutencaoVeiculo?Id={id}").Result;
+
+                if (response.IsSuccessStatusCode)
+                    return RedirectToAction(nameof(Index), new { mensagem = "Registro deletado!", sucesso = true });
+                else
+                    throw new Exception("Não foi possível carregar as informações!");
+            }
+            catch (Exception ex)
+            {
+                TempData["erro"] = $"Não foi possivel excluir o fornecedor " + ex.Message;
+                return View();
+            }
+        }
+
+        // POST: ManutencaoVeiculoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private List<SelectListItem> CarregarVeiculos()
+        {
+            List<SelectListItem> lista = new List<SelectListItem>();
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiToken.Obter());
+
+            HttpResponseMessage response = client.GetAsync($"{_dadosBase.Value.API_URL_BASE}Veiculo").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string conteudo = response.Content.ReadAsStringAsync().Result;
+                List<VeiculoModel> veiculos = JsonConvert.DeserializeObject<List<VeiculoModel>>(conteudo);
+
+                foreach (var linha in veiculos)
+                {
+                    lista.Add(new SelectListItem()
+                    {
+                        Value = linha.Placa.ToString(),
+                        Text = linha.Placa + " - " + linha.Modelo,
+                        Selected = false,
+                    });
+                }
+                return lista;
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+    }
+}
---
+++ CAR-LOCADORA/CarLocadora.Negocio/ManutencaoVeiculoNegocio/ManutencaoVeiculoNegocio.cs
@@ -0,0 +1,40 @@
+﻿using CarLocadora.Infra.Entity;
+using CarLocadora.Modelo;
+
+
+namespace CarLocadora.Negocio.ManutencaoVeiculo
+{
+    public class ManutencaoVeiculoNegocio : IManutencaoVeiculoNegocio
+    {
+        private readonly Context _context;
+        public ManutencaoVeiculoNegocio(Context context)
+        {
+            _context = context;
+        }
+
+        public void Alterar(ManutencaoVeiculoModel model)
+        {
+            model.DataAlteracao = DateTime.Now;
+            _context.Update(model);
+            _context.SaveChangesAsync();
+        }
+
+        public void Excluir(int id)
+        {
+            ManutencaoVeiculoModel model = _context.ManutencaoVeiculo.SingleOrDefault(x => x.Id.Equals(id));
+            _context.Remove(model);
+            _context.SaveChangesAsync();
+        }
+
+        public void Inserir(ManutencaoVeiculoModel model)
+        {
+            model.DataInclusao = DateTime.Now;
+            _context.AddAsync(model);
+            _context.SaveChangesAsync();
+        }
+        public ManutencaoVeiculoModel Obter(int id) => _context.ManutencaoVeiculo.SingleOrDefault(x => x.Id.Equals(id));
+
+        public List<ManutencaoVeiculoModel> ObterLista() => _context.ManutencaoVeiculo.ToList();
+
+    }
+}
